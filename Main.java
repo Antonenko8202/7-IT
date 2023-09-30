c96dfb14927b3c892a14173666742074f65f44e5
Antonenko8202 <144429776+Antonenko8202@users.noreply.github.com>
2023-09-30 12:43:37 +0300

    задача 3

---
+++ Main.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int num1 = scanner.nextInt();
+        int num2 = scanner.nextInt();
+        int num3 = scanner.nextInt();
+        int[] numbers = {num1, num2, num3};
+
+        Arrays.sort(numbers);
+        int temp;
+        temp = numbers[0];
+        numbers[0] = numbers[2];
+        numbers[2] = temp;
+
+
+        System.out.println(" " + numbers[0] + ", " + numbers[1] + ", " + numbers[2]);
+    }
+}
