f35afafe941d0c91dd69ee11f294dd6f5977efea
Antonenko8202 <144429776+Antonenko8202@users.noreply.github.com>
2023-09-30 12:48:22 +0300

    задача 4

--- Main.java
+++ Main.java
@@ -1,22 +1,19 @@
 import java.util.Scanner;
-import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        int num1 = scanner.nextInt();
-        int num2 = scanner.nextInt();
-        int num3 = scanner.nextInt();
-        int[] numbers = {num1, num2, num3};
+        System.out.print("Введите первое имя: ");
+        String name1 = scanner.nextLine();
 
-        Arrays.sort(numbers);
-        int temp;
-        temp = numbers[0];
-        numbers[0] = numbers[2];
-        numbers[2] = temp;
+        System.out.print("Введите второе имя: ");
+        String name2 = scanner.nextLine();
 
-
-        System.out.println(" " + numbers[0] + ", " + numbers[1] + ", " + numbers[2]);
+        if (name1.equals(name2)) {
+            System.out.println("Имена идентичные");
+        } else if (name1.length() == name2.length()) {
+            System.out.println("Длина имен одинаковая");
+        }
     }
 }
